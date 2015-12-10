7ef72fec766f39c0f6bad7bc7479b72cfbaa5627
Atreyee Mukhopadhyay <atreyee@linux.vnet.ibm.com>
2015-12-10 13:22:38 +0530

    Fixing alignment issues

--- ui/js/host-storage.js
+++ ui/js/host-storage.js
@@ -12,83 +12,6 @@ ginger.initStorage = function(){
     ginger.getFilesystems(function(result){
       var gridFields = [];
       var opts =[];
-      var addButton =[
-                  {
-                      id:'sd-add-FCP-button',
-                      class: 'fa fa-plus-circle',
-                      label: 'Add FCP Device',
-                      onClick: function(event) {
-                          wok.window.open('plugins/gingers390x/addFCPLuns.html');
-                      }
-                  },
-                  {
-                      id:'sd-add-ECKD-button',
-                      class: 'fa fa-plus-circle',
-                      label: 'Add ECKD Device',
-                      onClick: function(event) {
-                        // wok.window.open('plugins/gingerbase/report-add.html');
-                      }
-                  },
-                  {
-                      id:'sd-add-iSCSI-button',
-                      class: 'fa fa-plus-circle',
-                      label: 'Add iSCSI',
-                      onClick: function(event) {
-                          //wok.window.open('plugins/gingerbase/report-add.html');
-                      }
-                  }];
-
-
-
-        var actionButton = [{
-            id:'sd-add-VG-button',
-            class: 'fa fa-plus-circle',
-            label: 'Add to VG',
-            onClick: function(event) {
-                //wok.window.open('plugins/gingers390x/addFCPLuns.html');
-            }
-        },
-        {
-            id:'sd-format-button',
-            class: 'fa fa-pencil-square-o',
-            label: 'Format',
-            onClick: function(event) {
-              // wok.window.open('plugins/gingerbase/report-add.html');
-            }
-        },
-        {
-            id:'sd-partition-button',
-            class: 'fa fa-pie-chart',
-            label: 'Partition',
-            onClick: function(event) {
-                //wok.window.open('plugins/gingerbase/report-add.html');
-            }
-        },
-        {
-            id:'sd-remove-button',
-            class: 'fa fa-minus-circle',
-            label: 'Remove',
-            critical: true,
-            onClick: function(event) {
-                //wok.window.open('plugins/gingerbase/report-add.html');
-            }
-        }];
-        opts['addButtons'] = JSON.stringify(addButton);
-        opts['actions'] = JSON.stringify(actionButton);
-
-        var addListSettings = {
-             panelID : 'file-systems-add',
-             buttons : addButton,
-             type :'add'
-        };
-        var actionListSettings = {
-          panelID:'file-systems-actions',
-          buttons : actionButton,
-          type :'action'
-        };
-
-        ginger.createActionList(addListSettings);
-        ginger.createActionList(actionListSettings);
 
         opts['id']='file-systems';
         opts['gridId']= "fileSystemsGrid";
@@ -130,6 +53,7 @@ ginger.initStorage = function(){
         opts['gridFields']=JSON.stringify(gridFields);
 
         ginger.createGrid(opts);
+        ginger.loadActionButtons();
     });
     /*$("#addFCP").on("click",function(event){
      wok.window.open("plugins/gingers390x/network.html");
@@ -210,5 +134,79 @@ ginger.initStorage = function(){
 
      ginger.createGrid(opts);
    });
+   ginger.loadActionButtons = function(){
+   var addButton =[
+               {
+                   id:'sd-add-FCP-button',
+                   class: 'fa fa-plus-circle',
+                   label: 'Add FCP Device',
+                   onClick: function(event) {
+                       wok.window.open('plugins/gingers390x/addFCPLuns.html');
+                   }
+               },
+               {
+                   id:'sd-add-ECKD-button',
+                   class: 'fa fa-plus-circle',
+                   label: 'Add ECKD Device',
+                   onClick: function(event) {
+                     // wok.window.open('plugins/gingerbase/report-add.html');
+                   }
+               },
+               {
+                   id:'sd-add-iSCSI-button',
+                   class: 'fa fa-plus-circle',
+                   label: 'Add iSCSI',
+                   onClick: function(event) {
+                       //wok.window.open('plugins/gingerbase/report-add.html');
+                   }
+               }];
 
+ var actionButton = [{
+         id:'sd-add-VG-button',
+         class: 'fa fa-plus-circle',
+         label: 'Add to VG',
+         onClick: function(event) {
+             //wok.window.open('plugins/gingers390x/addFCPLuns.html');
+         }
+     },
+     {
+         id:'sd-format-button',
+         class: 'fa fa-pencil-square-o',
+         label: 'Format',
+         onClick: function(event) {
+           // wok.window.open('plugins/gingerbase/report-add.html');
+         }
+     },
+     {
+         id:'sd-partition-button',
+         class: 'fa fa-pie-chart',
+         label: 'Partition',
+         onClick: function(event) {
+             //wok.window.open('plugins/gingerbase/report-add.html');
+         }
+     },
+     {
+         id:'sd-remove-button',
+         class: 'fa fa-minus-circle',
+         label: 'Remove',
+         critical: true,
+         onClick: function(event) {
+             //wok.window.open('plugins/gingerbase/report-add.html');
+         }
+     }];
+
+     var addListSettings = {
+          panelID : 'file-systems-add',
+          buttons : addButton,
+          type :'add'
+     };
+     var actionListSettings = {
+       panelID:'file-systems-actions',
+       buttons : actionButton,
+       type :'action'
+     };
+
+     ginger.createActionList(addListSettings);
+     ginger.createActionList(actionListSettings);
+ }
 };
