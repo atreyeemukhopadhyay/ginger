9171aaf64511c780413474ab20fb7034aa3d6a41
Atreyee Mukhopadhyay <atreyee@linux.vnet.ibm.com>
2015-12-15 17:26:02 +0530

    Refactoring bootgrid related methods and integerating storage changes

--- ui/js/host-storage.js
+++ ui/js/host-storage.js
@@ -2,64 +2,13 @@
  *
  */
 ginger.initStorage = function(){
-    /*$("#storage-section").accordion({
-       event:"click",
-       collapsible:true,
-       active:0
-    });*/
  $(".content-area", "#storage-section").css("height", "100%");
 
-    ginger.getFilesystems(function(result){
-      var gridFields = [];
-      var opts =[];
-
-        opts['id']='file-systems';
-        opts['gridId']= "fileSystemsGrid";
-        opts['data']=JSON.stringify(result);
-        gridFields = [
-                      {
-                        "column-id": 'filesystem',
-                        "type": 'string',
-                        "width":"20%",
-                        "title": "Name"
-                      },
-                     {
-                       "column-id": 'mounted_on',
-                       "type": 'string',
-                       "identifier":true,
-                       "width":"20%",
-                       "title": "Mount Point"
-                      },
-                      {
-                        "column-id": 'type',
-                        "type": 'string',
-                        "width":"10%",
-                        "title": "Type"
-                      },
-                      {
-                        "column-id": 'size',
-                        "type": 'string',
-                        "width":"10%",
-                        "title": "Size"
-                      },
-                      {
-                        "column-id": "use%",
-                        "type": 'string',
-                        "title": "Usage",
-                        "width":"35%",
-                        "formatter": "percentage-used"
-                      }
-                  ];
-        opts['gridFields']=JSON.stringify(gridFields);
-
-        ginger.createGrid(opts);
-        ginger.loadActionButtons();
-    });
-    /*$("#addFCP").on("click",function(event){
-     wok.window.open("plugins/gingers390x/network.html");
-   });*/
-
-   ginger.getSwapdevices(function(result){
+   ginger.loadFileSystemDetails();
+   ginger.loadSwapDeviceDetails();
+   ginger.loadVolumeGroupDetails();
+   ginger.loadStorageDeviceDetails();
+   /*ginger.getSwapdevices(function(result){
      var gridFields = [];
      var opts =[];
      opts['id']='swap-devices';
@@ -133,80 +82,254 @@ ginger.initStorage = function(){
        opts['gridFields']=JSON.stringify(gridFields);
 
      ginger.createGrid(opts);
-   });
-   ginger.loadActionButtons = function(){
-   var addButton =[
-               {
-                   id:'sd-add-FCP-button',
-                   class: 'fa fa-plus-circle',
-                   label: 'Add FCP Device',
-                   onClick: function(event) {
-                       wok.window.open('plugins/gingers390x/addFCPLuns.html');
-                   }
-               },
-               {
-                   id:'sd-add-ECKD-button',
-                   class: 'fa fa-plus-circle',
-                   label: 'Add ECKD Device',
-                   onClick: function(event) {
-                     // wok.window.open('plugins/gingerbase/report-add.html');
-                   }
-               },
-               {
-                   id:'sd-add-iSCSI-button',
-                   class: 'fa fa-plus-circle',
-                   label: 'Add iSCSI',
-                   onClick: function(event) {
-                       //wok.window.open('plugins/gingerbase/report-add.html');
-                   }
-               }];
+   });*/
 
- var actionButton = [{
-         id:'sd-add-VG-button',
-         class: 'fa fa-plus-circle',
-         label: 'Add to VG',
-         onClick: function(event) {
-             //wok.window.open('plugins/gingers390x/addFCPLuns.html');
-         }
-     },
-     {
-         id:'sd-format-button',
-         class: 'fa fa-pencil-square-o',
-         label: 'Format',
-         onClick: function(event) {
-           // wok.window.open('plugins/gingerbase/report-add.html');
-         }
-     },
-     {
-         id:'sd-partition-button',
-         class: 'fa fa-pie-chart',
-         label: 'Partition',
-         onClick: function(event) {
-             //wok.window.open('plugins/gingerbase/report-add.html');
-         }
-     },
-     {
-         id:'sd-remove-button',
-         class: 'fa fa-minus-circle',
-         label: 'Remove',
-         critical: true,
-         onClick: function(event) {
-             //wok.window.open('plugins/gingerbase/report-add.html');
-         }
-     }];
-
-     var addListSettings = {
-          panelID : 'file-systems-add',
-          buttons : addButton,
-          type :'add'
-     };
-     var actionListSettings = {
-       panelID:'file-systems-actions',
-       buttons : actionButton,
-       type :'action'
-     };
-
-     ginger.createActionList(addListSettings);
-     ginger.createActionList(actionListSettings);
- }
 };
+ginger.loadStorageActionButtons = function(){
+var addButton =[
+            {
+                id:'sd-add-FCP-button',
+                class: 'fa fa-plus-circle',
+                label: 'Add FCP Device',
+                onClick: function(event) {
+                    wok.window.open('plugins/gingers390x/addFCPLuns.html');
+                }
+            },
+            {
+                id:'sd-add-ECKD-button',
+                class: 'fa fa-plus-circle',
+                label: 'Add ECKD Device',
+                onClick: function(event) {
+                  // wok.window.open('plugins/gingerbase/report-add.html');
+                }
+            },
+            {
+                id:'sd-add-iSCSI-button',
+                class: 'fa fa-plus-circle',
+                label: 'Add iSCSI',
+                onClick: function(event) {
+                    //wok.window.open('plugins/gingerbase/report-add.html');
+                }
+            }];
+
+var actionButton = [{
+      id:'sd-add-VG-button',
+      class: 'fa fa-plus-circle',
+      label: 'Add to VG',
+      onClick: function(event) {
+          //wok.window.open('plugins/gingers390x/addFCPLuns.html');
+      }
+  },
+  {
+      id:'sd-format-button',
+      class: 'fa fa-pencil-square-o',
+      label: 'Format',
+      onClick: function(event) {
+         var opts = [];
+         opts['gridId'] = "stgDevGrid";
+         opts['identifier'] = "id";
+      }
+  },
+  {
+      id:'sd-partition-button',
+      class: 'fa fa-pie-chart',
+      label: 'Partition',
+      onClick: function(event) {
+          //wok.window.open('plugins/gingerbase/report-add.html');
+      }
+  },
+  {
+      id:'sd-remove-button',
+      class: 'fa fa-minus-circle',
+      label: 'Remove',
+      critical: true,
+      onClick: function(event) {
+          //wok.window.open('plugins/gingerbase/report-add.html');
+      }
+  }];
+
+  var addListSettings = {
+       panelID : 'file-systems-add',
+       buttons : addButton,
+       type :'add'
+  };
+  var actionListSettings = {
+    panelID:'file-systems-actions',
+    buttons : actionButton,
+    type :'action'
+  };
+
+  ginger.createActionList(addListSettings);
+  ginger.createActionList(actionListSettings);
+};
+ginger.loadFileSystemDetails =  function(){
+  //ginger.loadStorageActionButtons();
+  var gridFields = [];
+  var opts =[];
+
+    opts['id']='file-systems';
+    opts['gridId']= "fileSystemsGrid";
+
+    gridFields = [
+                  {
+                    "column-id": 'filesystem',
+                    "type": 'string',
+                    "width":"20%",
+                    "title": "Name"
+                  },
+                 {
+                   "column-id": 'mounted_on',
+                   "type": 'string',
+                   "identifier":true,
+                   "width":"20%",
+                   "title": "Mount Point"
+                  },
+                  {
+                    "column-id": 'type',
+                    "type": 'string',
+                    "width":"10%",
+                    "title": "Type"
+                  },
+                  {
+                    "column-id": 'size',
+                    "type": 'string',
+                    "width":"10%",
+                    "title": "Size"
+                  },
+                  {
+                    "column-id": "use%",
+                    "type": 'string',
+                    "title": "Usage",
+                    "width":"35%",
+                    "formatter": "percentage-used"
+                  }
+              ];
+    opts['gridFields']=JSON.stringify(gridFields);
+
+    ginger.createBootgrid(opts);
+
+    ginger.getFilesystems(function(result){
+        ginger.loadBootgridData("fileSystemsGrid",result);
+    });
+};
+ginger.loadSwapDeviceDetails =  function(){
+  var gridFields = [];
+  var opts =[];
+  opts['id']='swap-devices';
+  opts['gridId']= "swapDevicesGrid";
+
+  gridFields = [
+                {
+                    "column-id":'filename',
+                    "type": 'string',
+                    "width":"20%",
+                    "title":'Name',
+                    "identifier":true
+                },
+               {
+                    "title": "Type",
+                    "column-id": 'type',
+                    "width":"15%",
+                    "type": 'string'
+                },
+                {
+                  "title": "Capacity",
+                  "column-id": "size",
+                  "width":"10%",
+                  "type": 'string'
+                },
+                {
+                  "title": "Usage",
+                  "column-id": "use_percent",
+                  "type": 'string',
+                  "width":"50%",
+                  "formatter": "percentage-used"
+                }
+            ];
+      opts['gridFields']=JSON.stringify(gridFields);
+      ginger.createBootgrid(opts);
+
+    ginger.getSwapdevices(function(result){
+        for (i=0; i < result.length; i++){
+          //calculate usage % from size and used (both are in bytes)
+          result[i]['use_percent']= (parseInt(result[i]['used'])/parseInt(result[i]['size'])) * 100;
+          result[i]['use_percent'] = result[i]['use_percent'].toFixed(2) + "%";
+          // convert size in bytes to readable format
+          result[i]['size'] = wok.formatMeasurement(parseInt(result[i]['size']), {
+                        fixed: 2});
+          result[i]['size'] = result[i]['size'].toString();
+         }
+          ginger.loadBootgridData("swapDevicesGrid",result);
+      });
+};
+ginger.loadVolumeGroupDetails= function(){
+   var gridFields = [];
+    var opts =[];
+    opts['id']='volume-groups';
+    opts['gridId']= "volumeGroupsGrid";
+
+    gridFields = [
+                    {
+                        "column-id": 'vgName',
+                        "type": 'string',
+                        "identifier":true,
+                        "width":"25%",
+                        "title": "Name"
+                    },
+                   {
+                     "column-id": 'vgSize',
+                     "type": 'string',
+                     "width":"70%",
+                     "title": "Capacity"
+                    }
+                ];
+      opts['gridFields']=JSON.stringify(gridFields);
+
+      ginger.createBootgrid(opts);
+
+      ginger.getVolumegroups(function(result){
+        ginger.loadBootgridData("volumeGroupsGrid",result);
+      });
+};
+ginger.loadStorageDeviceDetails = function(){
+     var gridFields = [];
+     var opts =[];
+     opts['id']='stg-devs';
+     opts['gridId']= "stgDevGrid";
+
+     ginger.loadStorageActionButtons();
+     gridFields = [
+                   {
+                       "column-id":'id',
+                       "type": 'string',
+                       "identifier":true,
+                       "width":"10%",
+                       "title":"ID"
+                   },
+                  {
+                       "column-id": 'mpath_count',
+                       "type": 'number',
+                       "width":"10%",
+                       "title":"Multipath Count"
+                   },
+                   {
+                     "column-id": 'type',
+                       "type": 'string',
+                       "width":"10%",
+                       "title":"Type"
+                   },
+                   {
+                     "column-id": 'size',
+                       "type": 'string',
+                       "width":"10%",
+                       "title":"Size"
+                   }
+               ];
+         opts['gridFields']=JSON.stringify(gridFields);
+
+         ginger.createBootgrid(opts);
+
+         ginger.getStgdevs(function(result){
+           ginger.loadBootgridData("stgDevGrid",result);
+         });
+}
--- ui/js/util.js
+++ ui/js/util.js
@@ -472,142 +472,16 @@ ginger.getPlugins = function(suc, err) {
         error: err
     });
 }
-ginger.createGrid =  function(opts){
-	var containerId = opts['id'];
-	var url = opts['url'];
-	var gridId=opts['gridId'];
-	var fields = JSON.parse(opts['gridFields']);
-	var data = JSON.parse(opts['data']);
-	var gridHtml = [
-	                '<table id="',gridId,'" class="table table-condensed table-hover table-striped" >',
-	                  '<thead>',
-		                   '<tr>',
-		                   '</tr>',
-	                  '</thead>'
-	               ].join('');
-	$(gridHtml).appendTo('#'+containerId);
-	var gridHeader = $('tr',gridHtml);
 
-	for(var i=0;i<fields.length;i++){
-
-		var columnHtml = [
-                      '<th data-type="',fields[i]["type"],'" data-column-id="',fields[i]["column-id"],'"',
-                       (fields[i].identifier)?'data-identifier="true"':'',' data-align="left" headerAlign="center"',
-                       ("formatter" in fields[i])?'data-formatter='+fields[i]["formatter"]:'',
-                       (fields[i].width)?(' data-width="'+fields[i].width+'"'):'',
-                       'data-header-css-class="gridHeader">',
-                      ("title" in fields[i])?fields[i]["title"]:fields[i]["column-id"],
-                      '</th>'
-		                  ].join('');
-		$(columnHtml).appendTo($('tr','#'+gridId));
-
-	}
-
-	var grid = $('#'+gridId).bootgrid({
-        selection: true,
-        multiSelect: true,
-        keepSelection: true,
-        rowCount:-1,
-        sorting:true,
-        columnSelection:false,
-        rowSelect:true,
-        formatters:{
-          "percentage-used" : function(column , row){
-          return '<div class="progress"><div class="progress-bar-info" style="width:'+row[column['id']]+'">'+row[column['id']]+'</div></div>';
-        },
-          "nw-interface-status": function(column, row)
-           {
-             var value = row[column.id];
-             if (column.id == "status") {
-               if (value == "up")
-                 return "<span class=\"nw-interface-status-enabled enabled\"> <i class=\"fa fa-power-off\"></i></span>";
-             return "<span class=\"nw-interface-status-disabled disabled\"> <i class=\"fa fa-power-off\"></i></span>";
-           }
-         },
-         "nw-address-space": function(column, row)
-          {
-            var ipaddr = row[column.id];
-            var netmask = row['netmask'];
-            if (!ipaddr) {
-              return "";
-            }
-            return ipaddr + "/" + netmask;
+ginger.getStgdevs =  function(suc , err){
+    wok.requestJSON({
+        url : 'plugins/ginger/stgdevs',
+        type : 'GET',
+        contentType : 'application/json',
+        dataType : 'json',
+        success : suc,
+        error : function(data) {
+            wok.message.error(data.responseJSON.reason);
         }
-       },
-        css:{
-            iconDown : "fa fa-sort-desc",
-            iconUp: "fa fa-sort-asc"
-         },
-         labels :{
-           search : "Filter"
-         }
-    }).on("load.rs.jquery.bootgrid", function (e) {
-        $('.input-group .glyphicon-search').removeClass('.glyphicon-search').addClass('fa fa-search');
-     });
-
-	grid.bootgrid("append",data);
-}
-ginger.getSelectedRowsData = function(opts){
-  var selectedRowDetails = [];
-  var currentRows = ginger.getCurrentRows(opts);
-  var selectedRowIds = ginger.getSelectedtRows(opts);
-  var identifier = opts['identifier'];
-  $.each(currentRows,function(i,row){
-    var rowDetails = row;
-    if(selectedRowIds.indexOf(rowDetails[identifier])!=-1){
-      selectedRowDetails.push(rowDetails);
-    }
-  });
-  return selectedRowDetails;
-};
-
-ginger.getCurrentRows =  function(opts){
-	return $('#'+opts['gridId']).bootgrid("getCurrentRows");
-}
-
-ginger.getSelectedtRows =  function(opts){
-	return $('#'+opts['gridId']).bootgrid("getSelectedRows");
-}
-
-ginger.getTotalRowCount =  function(opts){
-	return $('#'+opts['gridId']).bootgrid("getTotalRowCount");
-}
-
-ginger.reloadGridData =  function(opts){
-	return $('#'+opts['gridId']).bootgrid("reload");
-}
-ginger.createActionList = function(settings){
-  var toolbarNode = null;
-  var btnHTML, dropHTML = [];
-  var container = settings.panelID;
-  var toolbarButtons = settings.buttons;
-  var buttonType = settings.type;
-  toolbarNode = $('<div class="btn-group"></div>');
-  toolbarNode.appendTo($("#"+container));
-  dropHTML = ['<div class="dropdown menu-flat">',
-                      '<button id="action-dropdown-button-', container, '" class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">',
-                       (buttonType==='action')?'<span class="edit-alt"></span>Actions':'<i class="fa fa-plus-circle"></i>Add ','<span class="caret"></span>',
-                      '</button>',
-                      '<ul class="dropdown-menu"></ul>',
-                      '</div>'
-                  ].join('');
-   $(dropHTML).appendTo(toolbarNode);
-
-     $.each(toolbarButtons, function(i, button) {
-                    var btnHTML = [
-                        '<li role="presentation"', button.critical === true ? ' class="critical"' : '', '>',
-                        '<a role="menuitem" tabindex="-1" data-dismiss="modal"', (button.id ? (' id="' + button.id + '"') : ''), (button.disabled === true ? ' class="disabled"' : ''),
-                        '>',
-                        button.class ? ('<i class="' + button.class) + '"></i>' : '',
-                        button.label,
-                        '</a></li>'
-                    ].join('');
-                    var btnNode = $(btnHTML).appendTo($('.dropdown-menu', toolbarNode));
-                    button.onClick && btnNode.on('click', button.onClick);
-                });
-}
-ginger.changeButtonStatus = function(buttonIds, state){
-  $.each(buttonIds, function(i, buttonId) {
-     $('#'+buttonId).prop("disabled", state);
-  });
+    });
 }
